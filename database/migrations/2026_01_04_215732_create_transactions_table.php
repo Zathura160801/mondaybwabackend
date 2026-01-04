939bbe8231414611f83ecaad11c76c4c0a9fea54
Zathura160801 <adipz160801@gmail.com>
2026-01-04 23:07:45 +0800

    chore(db): add new columns through migrations
    - users
    - categories
    - products
    - warehouses
    - merchants
    - merchant_products
    - warehouse_products
    - transactions
    - transaction_products

--- database/migrations/2026_01_04_215732_create_transactions_table.php
+++ database/migrations/2026_01_04_215732_create_transactions_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->string('phone')->unique();
+            $table->unsignedInteger('sub_total');
+            $table->unsignedInteger('tax_total');
+            $table->unsignedInteger('grand_total')->index();
+            $table->foreignId('merchant_id')->constrained()->onDelete('cascade');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
