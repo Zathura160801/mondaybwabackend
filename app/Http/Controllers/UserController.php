13b5f306f906b7bee76b5a35797bde63706c986b
Zathura160801 <adipz160801@gmail.com>
2026-01-06 04:58:54 +0800

    feat(user): add controller, service, repository, request, and resource

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Http\Resources\UserResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+
+class UserController extends Controller
+{
+    private $userService;
+
+    public function __construct(UserService $userService)
+    {
+        $this->userService = $userService;
+    }
+
+    public function index()
+    {
+        $fields = ['id', 'name', 'email', 'photo', 'phone'];
+
+        $users = $this->userService->getAll($fields ?: ['*']);
+
+        return response()->json(UserResource::collection($users));
+    }
+
+    public function show(int $id)
+    {
+        $fields = ['id', 'name', 'photo', 'tagline'];
+
+        $user = $this->userService->getById($id, $fields ?: ['*']);
+
+        return response()->json(new UserResource($user));
+    }
+
+    public function store(UserRequest $request)
+    {
+        $user = $this->userService->create($request->validated());
+
+        return response()->json(new UserResource($user), 201);
+    }
+
+    public function update(UserRequest $request, int $id)
+    {
+        $user = $this->userService->update($id, $request->validated());
+
+        return response()->json(new UserResource($user));
+    }
+
+    public function destroy(int $id)
+    {
+        $this->userService->delete($id);
+
+        return response()->json(['message' => 'User deleted successfully']);
+    }
+}
