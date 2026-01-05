719b9794977f2169957709940b1b50ff0f253c9f
Zathura160801 <adipz160801@gmail.com>
2026-01-06 04:34:05 +0800

    feat(product): add controller, service, repository, request, and resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,78 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class ProductController extends Controller
 {
-    //
+    private $productService;
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+
+    public function index()
+    {
+        $fields = ['id', 'name', 'thumbnail', 'price', 'category_id'];
+
+        $products = $this->productService->getAll($fields);
+
+        return response()->json(ProductResource::collection($products));
+    }
+
+    public function show(int $id)
+    {
+        try {
+            $fields = ['id', 'name', 'thumbnail', 'price', 'about', 'category_id'];
+
+            $product = $this->productService->getById($id, $fields);
+
+            return response()->json(new ProductResource($product));
+            
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Product not found'
+            ], 404);
+        }
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $product = $this->productService->create($request->validated());
+
+        return response()->json(new ProductResource($product), 201);
+    }
+
+    public function update(ProductRequest $request, int $id)
+    {
+        try {
+            $product = $this->productService->update($id, $request->validated());
+
+            return response()->json(new ProductResource($product));
+
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Product not found'
+            ], 404);
+        }
+    }
+
+    public function destroy(int $id)
+    {
+        try {
+            $this->productService->delete($id);
+
+            return response()->json([
+                'message' => 'Product deleted successfully'
+            ]);
+
+        } catch (ModelNotFoundException $e) {
+            return response()->json([
+                'message' => 'Product not found'
+            ], 404);
+        }
+    }
 }
---
+++ app/Services/ProductService.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\UploadedFile;
+use App\Repositories\ProductRepository;
+
+class ProductService
+{
+    protected $productRepository;
+
+    public function __construct(ProductRepository $productRepository)
+    {
+        $this->productRepository = $productRepository;
+    }
+
+    public function getAll(array $fields)
+    {
+        return $this->productRepository->getAll($fields);
+    }
+
+    public function getById(int $id, array $fields)
+    {
+        return $this->productRepository->getById($id, $fields ?? ['*']);
+    }
+
+    public function create(array $data)
+    {
+        if (isset($data['thumbnail']) && $data['thumbnail'] instanceof UploadedFile) {
+            $data['thumbnail'] = $this->uploadPhoto($data['thumbnail']);
+        }
+
+        return $this->productRepository->create($data);
+    }
+
+    public function update(int $id, array $data)
+    {
+        $fields = ['id'];
+
+        $product = $this->productRepository->getById($id, $fields);
+
+        if (isset($data['thumbnail']) && $data['thumbnail'] instanceof UploadedFile) {
+            if (!empty($product->thumbnail)) {
+                $this->deletePhoto($product->thumbnail);
+            }
+
+            $data['thumbnail'] = $this->uploadPhoto($data['thumbnail']);
+        }
+
+        return $this->productRepository->update($id, $data);
+    }
+
+    public function delete(int $id)
+    {
+        $fields = ['id', 'thumbnail'];
+
+        $product = $this->productRepository->getById($id, $fields);
+
+        if ($product->thumbnail) {
+            $this->deletePhoto($product->thumbnail);
+        }
+
+        $this->productRepository->delete($id);
+    }
+
+    public function uploadPhoto(UploadedFile $photo)
+    {
+        return $photo->store('products', 'public');
+    }
+
+    public function deletePhoto(string $photoPath)
+    {
+        $relativePath = 'products/' . basename($photoPath);
+        if (Storage::disk('public')->exists($relativePath)) {
+            Storage::disk('public')->delete($relativePath);
+        }
+    }
+}
